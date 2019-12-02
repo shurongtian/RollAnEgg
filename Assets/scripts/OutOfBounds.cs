6ddad5c27a82587fdcb31a336353a477d038a17e
Kyle Bolton <latestthing@gmail.com>
2019-12-01 21:12:36 -0600

    added out of bounds and finish is WIP

---
+++ Assets/scripts/OutOfBounds.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OutOfBounds : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Player")){
+             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
